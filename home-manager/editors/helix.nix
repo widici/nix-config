8a796f63f12974e9a68a16c9da36416e655528d5
widici <84205124+widici@users.noreply.github.com>
2026-02-14 23:06:23 +0000

    Added a relatively minimal helix config

--- home-manager/editors/helix.nix
+++ home-manager/editors/helix.nix
@@ -1,8 +1,40 @@
+# TODO: add shell
+
 { ... }:
 
+let
+  # TODO: potentially rework this to use caps
+  normalModeKey = {
+    f.j = "normal_mode";
+  };
+in
 {
   programs.helix = {
     enable = true;
     defaultEditor = true;
+
+    settings = {
+      theme = "catppuccin_mocha";
+
+      editor = {
+        line-number = "relative";
+        bufferline = "multiple";
+        true-color = true;
+        auto-format = true;
+
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+
+        soft-wrap.enable = true;
+      };
+
+      keys = {
+        select = normalModeKey;
+        insert = normalModeKey;
+      };
+    };
   };
 }
