4e443a3881b2882a2841cd3541c52a9beaedc05f
widici <84205124+widici@users.noreply.github.com>
2026-07-30 22:33:10 +0000

    feat: swtiched from flake-utils -> flake-parts for the minimal shell template

--- templates/minimal/flake.nix
+++ templates/minimal/flake.nix
@@ -3,20 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    systems.url = "github:nix-systems/default";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (sys:
-      let
-        pkgs = import nixpkgs { system = sys; };
-        deps = [ pkgs.cowsay ];
-        package = pkgs.writeShellScriptBin "program" ''${pkgs.cowsay}/bin/cowsay "hello world! :D"'';
-      in {
-        packages.default = package;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      perSystem = { pkgs, ... }: {
+        packages.default = 
+pkgs.writeShellScriptBin "program" ''${pkgs.cowsay}/bin/cowsay "hello world! :D"'';
+
         devShells.default = pkgs.mkShell {
-          packages = deps;
+          packages = [pkgs.cowsay];
         };
-      }
-    );  
+      };
+    };
 }
